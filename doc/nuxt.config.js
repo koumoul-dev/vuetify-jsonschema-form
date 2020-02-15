c49e2dfe01ba5a1dd10968c2159733cc63677e07
Alban Mouton <alban.mouton@gmail.com>
2020-02-15 21:43:14 +0100

    fix: broken scroll to hash

--- doc/nuxt.config.js
+++ doc/nuxt.config.js
@@ -27,7 +27,34 @@ module.exports = {
     }
   },
   router: {
-    base: process.env.BASE || '/'
+    base: process.env.BASE || '/',
+    // cf https://toor.co/blog/nuxtjs-smooth-scrolling-with-hash-links/
+    scrollBehavior: async (to, from, savedPosition) => {
+      if (savedPosition) {
+        return savedPosition
+      }
+
+      const findEl = async (hash, x) => {
+        return document.querySelector(hash) ||
+          new Promise((resolve, reject) => {
+            if (x > 50) {
+              return resolve()
+            }
+            setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
+          })
+      }
+
+      if (to.hash) {
+        const el = await findEl(to.hash)
+        if ('scrollBehavior' in document.documentElement.style) {
+          return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
+        } else {
+          return window.scrollTo(0, el.offsetTop)
+        }
+      }
+
+      return { x: 0, y: 0 }
+    }
   },
   head: {
     title: 'vuetify-jsonschema-form - documentation',
