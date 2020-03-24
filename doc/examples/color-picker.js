faf22d575d2cf8d8dffddbdacdbcdc13b5d18095
Alban Mouton <alban.mouton@gmail.com>
2020-03-24 23:31:45 +0100

    chore: work on about page

--- doc/examples/color-picker.js
+++ doc/examples/color-picker.js
@@ -2,9 +2,9 @@ const id = 'colors'
 
 const title = 'Colors'
 
-const description = `The format "hexcolor" is used to display a color picker.
+const description = `The format \`hexcolor\` is used to display a color picker. Please not that it is not a standard format (I'm pretty sure it was once a proposal) and you might need to declare it with your JSON schema validator.
 
-Default uses [Vue Swatches](https://saintplay.github.io/vue-swatches/) but you can switch to a more complete color picket using [vue-color](https://github.com/xiaokaike/vue-color).
+Default uses [Vue Swatches](https://saintplay.github.io/vue-swatches/) but you can switch to a more complete color picker using [vue-color](https://github.com/xiaokaike/vue-color) with the \`x-display="color-picker"\` annotation.
 `
 
 const schema = {
