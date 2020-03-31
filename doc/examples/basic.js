6ea02d5a160f8b9fc0e1f78a3d77a42076eeab05
Alban Mouton <alban.mouton@gmail.com>
2020-03-31 22:51:01 +0200

    chore: textarea and slider examples

--- doc/examples/basic.js
+++ doc/examples/basic.js
@@ -8,8 +8,10 @@ const schema = {
   type: 'object',
   properties: {
     stringProp: { type: 'string', title: `I'm a string`, description: 'This description is used as a help message.' },
+    stringTextareaProp: { type: 'string', title: `I'm a string in a textarea`, 'x-display': 'textarea' },
     numberProp: { type: 'number', title: `I'm a number` },
     integerProp: { type: 'integer', title: `I'm an integer` },
+    integerSliderProp: { type: 'integer', title: `I'm an integer in a slider`, 'x-display': 'slider', minimum: 0, maximum: 5 },
     booleanProp: { type: 'boolean', title: `I'm a boolean` },
     stringArrayProp: { type: 'array', title: `I'm an array of strings`, items: { type: 'string' } }
   }
