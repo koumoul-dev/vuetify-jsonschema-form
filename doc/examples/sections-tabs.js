2b511ac3f1f4c40db08cb89a87a05f8f73bd9afa
Alban Mouton <alban.mouton@gmail.com>
2020-04-01 15:22:14 +0200

    fix: missing key in allOf schemas

--- doc/examples/sections-tabs.js
+++ doc/examples/sections-tabs.js
@@ -11,23 +11,21 @@ const schema = {
   'x-display': 'tabs',
   'x-props': { grow: true },
   description: `I'm an object with sections rendered as tabs.`,
-  properties: {
-    objectSection1: {
-      title: `I'm a section`,
-      description: `I'm a description shown as a paragraph on top of section`,
-      type: 'object',
-      properties: {
-        stringProp1: { type: 'string', title: `I'm a property in section 1` }
-      }
-    },
-    objectSection2: {
-      title: `I'm another section`,
-      type: 'object',
-      properties: {
-        stringProp2: { type: 'string', title: `I'm a property in section 2` }
-      }
+  allOf: [{
+    title: `I'm a section`,
+    description: `I'm a description shown as a paragraph on top of section`,
+    type: 'object',
+    properties: {
+      stringProp1: { type: 'string', title: `I'm a property in section 1` }
     }
-  }
+  },
+  {
+    title: `I'm another section`,
+    type: 'object',
+    properties: {
+      stringProp2: { type: 'string', title: `I'm a property in section 2` }
+    }
+  }]
 }
 
 const model = {}
