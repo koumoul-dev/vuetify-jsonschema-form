14c3e90caa86ffbcef4c306bd96883231ac44967
Alban Mouton <alban.mouton@gmail.com>
2020-04-02 11:50:35 +0200

    fix: use exact version from cdn in codepen example to prevent caching issues

--- doc/components/example-wrapper.vue
+++ doc/components/example-wrapper.vue
@@ -107,6 +107,7 @@
 
 import Example from './example.js'
 import { defaultTemplate } from '../examples'
+import pJson from '../../package.json'
 const md = require('markdown-it')()
 const stringifyObject = require('stringify-object')
 const Ajv = require('ajv')
@@ -199,8 +200,8 @@ new Vue({
     valid: null
   }
 });`,
-        css_external: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900;https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css;https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@1/dist/main.css',
-        js_external: 'https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js;https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js;https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@1/dist/main.js;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@1/dist/third-party.js'
+        css_external: `https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900;https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css;https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@${pJson.version}/dist/main.css`,
+        js_external: `https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js;https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js;https://cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@${pJson.version}/dist/main.js;https://cdn.jsdelivr.net/npm/@koumoul/vjsf@${pJson.version}/dist/third-party.js`
       }
     }
   },
