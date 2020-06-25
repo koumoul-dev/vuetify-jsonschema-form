6fdb820f5a0466157cf1dfdf829ae0305278b1ff
Alban Mouton <alban.mouton@gmail.com>
2020-06-25 21:27:29 +0200

    fix: broken getting-started page

--- doc/pages/getting-started.vue
+++ doc/pages/getting-started.vue
@@ -9,34 +9,34 @@
     </h2>
     <!-- eslint-disable vue/html-indent -->
     <v-sheet v-hljs dark class="codeblock">
-      <code class="html">&lt;script src="https://cdn.jsdelivr.net/npm/@koumoul/vjsf@latest/dist/main.js"&gt;&lt;/script&gt;
+      <pre><code class="html">&lt;script src="https://cdn.jsdelivr.net/npm/@koumoul/vjsf@latest/dist/main.js"&gt;&lt;/script&gt;
 &lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@koumoul/vjsf@latest/dist/main.css"&gt;
 &lt;!-- load third-party dependencies (markdown-it, vuedraggable) --&gt;
 &lt;!-- you can also load them separately based on your needs --&gt;
-&lt;script src="https://cdn.jsdelivr.net/npm/@koumoul/vjsf@latest/dist/third-party.js"&gt;&lt;/script&gt;</code>
-    </v-sheet>
+&lt;script src="https://cdn.jsdelivr.net/npm/@koumoul/vjsf@latest/dist/third-party.js"&gt;&lt;/script&gt;</code></pre>
+</v-sheet>
     <v-sheet v-hljs dark class="codeblock">
-      <code class="javascript">Vue.component('VJsf', VJsf.default)</code>
+      <pre><code class="javascript">Vue.component('VJsf', VJsf.default)</code></pre>
     </v-sheet>
 
     <h2 class="headline my-4">
       Load from NPM
     </h2>
     <v-sheet v-hljs dark class="codeblock">
-      <code class="bash">npm install --save @koumoul/vjsf</code>
+      <pre><code class="bash">npm install --save @koumoul/vjsf</code></pre>
     </v-sheet>
 
     <h3 class="title my-4">
       Import built version of the module
     </h3>
     <v-sheet v-hljs dark class="codeblock">
-      <code class="javascript">import VJsf from '@koumoul/vjsf'
+      <pre><code class="javascript">import VJsf from '@koumoul/vjsf'
 import '@koumoul/vjsf/dist/main.css'
 // load third-party dependencies (markdown-it, vuedraggable)
 // you can also load them separately based on your needs
 import '@koumoul/vjsf/dist/third-party.js'
 
-Vue.component('VJsf', VJsf)</code>
+Vue.component('VJsf', VJsf)</code></pre>
     </v-sheet>
 
     <h3 class="title my-4">
@@ -46,20 +46,20 @@ Vue.component('VJsf', VJsf)</code>
       This is useful if you wish to let your build tool analyze the source code of vjsf. Particularily interesting if you use vuetify-loader.
     </p>
     <v-sheet v-hljs dark class="codeblock">
-      <code class="javascript">import VJsf from '@koumoul/vjsf/lib/VJsf.js'
+      <pre><code class="javascript">import VJsf from '@koumoul/vjsf/lib/VJsf.js'
 import '@koumoul/vjsf/lib/VJsf.css'
 // load third-party dependencies (markdown-it, vuedraggable)
 // you can also load them separately based on your needs
 import '@koumoul/vjsf/lib/deps/third-party.js'
 
-Vue.component('VJsf', VJsf)</code>
+Vue.component('VJsf', VJsf)</code></pre>
     </v-sheet>
     <v-sheet v-hljs dark class="codeblock">
-      <code class="javascript">// you should have something like the following somewhere in webpack.config.js or nuxt.config.js, etc.
+      <pre><code class="javascript">// you should have something like the following somewhere in webpack.config.js or nuxt.config.js, etc.
 build: {
   transpile: ['vuetify', /@koumoul/], // necessary for "à la carte" import of vuetify components
 }
-      </code>
+</code></pre>
     </v-sheet>
 
     <h2 class="headline my-4">
@@ -67,9 +67,9 @@ build: {
     </h2>
 
     <v-sheet v-hljs dark class="codeblock">
-      <code class="html">&lt;v-form v-model="valid"&gt;
+      <pre><code class="html">&lt;v-form v-model="valid"&gt;
   &lt;v-jsf v-model="model" :schema="schema" :options="options" /&gt;
-&lt;/ v-form&gt;</code>
+&lt;/ v-form&gt;</code></pre>
     </v-sheet>
     <!-- eslint-enable vue/html-indent -->
   </v-container>
