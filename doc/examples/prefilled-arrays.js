3eef214ba85fac5068f8a3ac7608d3dc623e43a0
Alban Mouton <alban.mouton@gmail.com>
2021-01-02 12:54:34 +0100

    chore: add mocks to prefilled array example

--- doc/examples/prefilled-arrays.js
+++ doc/examples/prefilled-arrays.js
@@ -119,7 +119,29 @@ const httpMocks = {
       previews: [],
       href: 'https://koumoul.com/s/data-fair/api/v1/datasets/jep-2018-france',
       page: 'https://koumoul.com/s/data-fair/dataset/jep-2018-france'
-    }] }
+    }]
+  },
+  'https://koumoul.com/s/data-fair/api/v1/datasets/icons-mdi-latest/lines?q=': {
+    total: 4495,
+    results: [{
+      path: 'ab-testing/ab-testing.svg',
+      '_file.content_type': 'image/svg+xml',
+      '_file.content': '',
+      '_file.content_length': 3,
+      _rand: 132845,
+      svg: '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-ab-testing" width="24" height="24" viewBox="0 0 24 24"><path d="M4 2A2 2 0 0 0 2 4V12H4V8H6V12H8V4A2 2 0 0 0 6 2H4M4 4H6V6H4M22 15.5V14A2 2 0 0 0 20 12H16V22H20A2 2 0 0 0 22 20V18.5A1.54 1.54 0 0 0 20.5 17A1.54 1.54 0 0 0 22 15.5M20 20H18V18H20V20M20 16H18V14H20M5.79 21.61L4.21 20.39L18.21 2.39L19.79 3.61Z" /></svg>',
+      author: 'Michael Richins',
+      name: 'ab-testing',
+      _i: 1569936485740,
+      packVersion: '4.4.95',
+      _attachment_url: 'https://koumoul.com/s/data-fair/api/v1/datasets/icons-mdi-4.4.95/attachments/ab-testing/ab-testing.svg',
+      version: '4.0.96',
+      pack: 'mdi',
+      tags: 'Developer / Languages',
+      _score: null,
+      _id: 'ab-testing'
+    }]
+  }
 }
 
 export default { id, title, description, schema, model, options, httpMocks }
