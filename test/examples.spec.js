f79ec7ac902541ec46211da1bcd130a8cfea651a
Alban Mouton <alban.mouton@gmail.com>
2021-04-08 21:49:07 +0200

    fix: broken tests

--- test/examples.spec.js
+++ test/examples.spec.js
@@ -2,14 +2,17 @@ import { examples } from '../doc/examples'
 const utils = require('./utils')
 
 describe('Examples used as simple test cases', () => {
-  examples.filter(example => !example.skip).forEach(example => {
-    test(example.title, () => {
-      const wrapper = utils.getExampleWrapper(example)
-      // wait a little bit for optional asynchronous calls
-      return new Promise(resolve => setTimeout(resolve, 200)).then(() => {
-        expect(wrapper.element).toMatchSnapshot()
-        if (example.test) example.test(wrapper)
+  for (const examplesGroup of examples) {
+    for (const example of examplesGroup.examples) {
+      if (example.skip) continue
+      test(example.title, () => {
+        const wrapper = utils.getExampleWrapper(example)
+        // wait a little bit for optional asynchronous calls
+        return new Promise(resolve => setTimeout(resolve, 200)).then(() => {
+          expect(wrapper.element).toMatchSnapshot()
+          if (example.test) example.test(wrapper)
+        })
       })
-    })
-  })
+    }
+  }
 })
