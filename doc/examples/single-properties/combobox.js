78acaaa8aea6504c4850397cba21803174bb7495
Alban Mouton <alban.mouton@gmail.com>
2021-06-03 14:15:38 +0200

    fix: broken test

--- doc/examples/single-properties/combobox.js
+++ doc/examples/single-properties/combobox.js
@@ -61,4 +61,8 @@ const model = {}
 
 const options = { context: { owner: { type: 'organization', id: '5a5dc47163ebd4a6f438589b' } } }
 
-export default { id, title, description, schema, model, options }
+const httpMocks = {
+  'https://koumoul.com/s/data-fair/api/v1/datasets?status=finalized&select=title&owner=organization:5a5dc47163ebd4a6f438589b': { results: [] }
+}
+
+export default { id, title, description, schema, model, options, httpMocks }
