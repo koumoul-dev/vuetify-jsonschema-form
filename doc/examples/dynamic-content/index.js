47a61df8aab441f5a4d128d5328596caae822790
Alban Mouton <alban.mouton@gmail.com>
2021-09-15 18:04:40 +0200

    feat: work on advanced expression

--- doc/examples/dynamic-content/index.js
+++ doc/examples/dynamic-content/index.js
@@ -1,13 +1,15 @@
 import selectSubschemas from './select-subschemas'
 import conditionals from './conditionals'
 import selectDeps from './select-deps'
+import selectContextDeps from './select-context-deps'
 
 const examplesGroup = {
   title: 'Dynamic content',
   examples: [
     selectSubschemas,
     conditionals,
-    selectDeps
+    selectDeps,
+    selectContextDeps
   ]
 }
 
---
+++ doc/examples/dynamic-content/select-context-deps.js
@@ -0,0 +1,41 @@
+const id = 'select-context-deps'
+
+const title = 'Selects with context dependencies'
+
+const description = `It is possible to create a select based on values that depend both on other parts of the model and on the context.`
+
+const schema = {
+  type: 'object',
+  properties: {
+    selectCategory: {
+      type: 'string',
+      title: `I'm a string from a select that is used to suggest values in the next select`,
+      enum: ['cat1', 'cat2']
+    }
+  },
+  dependencies: {
+    selectCategory: {
+      properties: {
+        selectFromContext: {
+          type: 'string',
+          title: `I'm a string from a select whose values come from the context and depend on the previous select`,
+          'x-fromData': 'context.categoryValues[modelRoot.selectCategory]'
+        }
+      }
+    }
+  }
+}
+
+const model = {}
+
+const options = {
+  useNewFunction: true,
+  context: {
+    categoryValues: {
+      cat1: ['c1-v1', 'c1-v2'],
+      cat2: ['c2-v1', 'c2-v2']
+    }
+  }
+}
+
+export default { id, title, description, schema, model, options }
