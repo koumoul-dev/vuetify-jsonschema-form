f1790a334f25cfd5b50ddb2a42b61d1130d10ecb
Alban Mouton <alban.mouton@gmail.com>
2022-01-24 11:23:54 +0100

    feat: add relative dependencies

--- doc/examples/dynamic-content/index.js
+++ doc/examples/dynamic-content/index.js
@@ -2,6 +2,7 @@ import selectSubschemas from './select-subschemas'
 import conditionals from './conditionals'
 import conditionalExpr from './conditional-expr'
 import selectDeps from './select-deps'
+import selectRelativeDeps from './select-relative-deps'
 import selectContextDeps from './select-context-deps'
 
 const examplesGroup = {
@@ -11,6 +12,7 @@ const examplesGroup = {
     conditionals,
     conditionalExpr,
     selectDeps,
+    selectRelativeDeps,
     selectContextDeps
   ]
 }
