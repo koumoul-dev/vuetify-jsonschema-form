a5a9af0d24343a9989e528e3cede952193ada28c
Alban Mouton <alban.mouton@gmail.com>
2022-04-20 21:22:53 +0200

    docs: fix examples

--- doc/components/example.js
+++ doc/components/example.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Draggable from 'vuedraggable'
 import '../../lib/deps/third-party.js'
-import VJsf from '../../lib/VJsfNoDeps.js'
+import VJsf from '../../lib/VJsf.js'
 import '../../lib/VJsf.css'
 import EasyMDE from 'easymde/dist/easymde.min.js'
 import 'easymde/dist/easymde.min.css'
