457492920f8cf3fc8557fa62fded849249146686
Alban Mouton <alban.mouton@gmail.com>
2023-01-05 10:20:35 +0100

    docs: remove robots meta with noindex value

--- doc/nuxt.config.js
+++ doc/nuxt.config.js
@@ -48,8 +48,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: `vjsf - Documentation` },
-      { hid: 'robots', name: 'robots', content: 'noindex' }
+      { hid: 'description', name: 'description', content: `vjsf - Documentation` }
     ],
     link: [{
       rel: 'stylesheet',
