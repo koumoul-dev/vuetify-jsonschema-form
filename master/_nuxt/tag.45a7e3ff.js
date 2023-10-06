87e68ad93a318e6a4780668e919cd154a9578d06
Alban Mouton <alban.mouton@gmail.com>
2023-10-06 11:32:20 +0200

    Pushed master by gh-pages-multi

--- master/_nuxt/tag.45a7e3ff.js
+++
@@ -1,1 +0,0 @@
-import{H as t,aY as a}from"./entry.9b03b506.js";const s=t({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component");function o(e){const r=a("useRender");r.render=e}const p=t({tag:{type:String,default:"div"}},"tag");export{p as a,s as m,o as u};
---
+++ master/_nuxt/tag.ddf107cf.js
@@ -0,0 +1,1 @@
+import{Z as t,ay as a}from"./entry.bb0bdacd.js";const s=t({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component");function o(e){const r=a("useRender");r.render=e}const p=t({tag:{type:String,default:"div"}},"tag");export{p as a,s as m,o as u};
