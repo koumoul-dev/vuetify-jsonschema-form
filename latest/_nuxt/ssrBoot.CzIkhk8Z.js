76f0dd5a7466a191debd186c3552ef0434f5f565
Alban Mouton <alban.mouton@gmail.com>
2024-03-08 17:06:59 +0100

    Pushed latest by gh-pages-multi

---
+++ latest/_nuxt/ssrBoot.CzIkhk8Z.js
@@ -0,0 +1,1 @@
+import{ab as t,o as e,c as s,aB as n}from"./entry.DTIvWivT.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
