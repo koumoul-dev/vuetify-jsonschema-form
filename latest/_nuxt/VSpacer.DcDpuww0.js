70027d7c8994e84b0c705caa0a180a5cbf7c9df1
Alban Mouton <alban.mouton@gmail.com>
2024-05-16 16:25:49 +0200

    Pushed latest by gh-pages-multi

--- latest/_nuxt/VSpacer.DcDpuww0.js
+++
@@ -1,1 +0,0 @@
-import{aB as o}from"./entry.DolQEcmB.js";/* empty css              */import{c}from"./index.CyoKxxFu.js";function p(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}const a=c("v-spacer","div","VSpacer");export{a as V,p as u};
---
+++ latest/_nuxt/tag.C9N9Cgx7.js
@@ -0,0 +1,1 @@
+import{N as t,aB as a}from"./entry.BZX4tnh6.js";const s=t({class:[String,Array],style:{type:[String,Array,Object],default:null}},"component");function o(e){const r=a("useRender");r.render=e}const p=t({tag:{type:String,default:"div"}},"tag");export{p as a,s as m,o as u};
