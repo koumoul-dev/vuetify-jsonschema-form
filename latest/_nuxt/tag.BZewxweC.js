84aac04798d3fb2b726e5d9e89376b26d1a6a311
Alban Mouton <alban.mouton@gmail.com>
2024-06-03 16:34:37 +0200

    Pushed latest by gh-pages-multi

---
+++ latest/_nuxt/tag.BZewxweC.js
@@ -0,0 +1,1 @@
+import{N as t,ap as a}from"./entry.B8oTM9XL.js";const s=t({class:[String,Array,Object],style:{type:[String,Array,Object],default:null}},"component");function o(e){const r=a("useRender");r.render=e}const p=t({tag:{type:String,default:"div"}},"tag");export{p as a,s as m,o as u};
