fce2f411ca8bcfc8bc188bfcfa2b93eec8712391
Alban Mouton <alban.mouton@gmail.com>
2025-02-12 14:26:18 +0100

    Pushed master by gh-pages-multi

---
+++ master/_nuxt/rH73eVOi.js
@@ -0,0 +1,1 @@
+import{O as n,$ as r,i as u,ac as l}from"./BDx8aAHU.js";const f=n({eager:Boolean},"lazy");function i(e,o){const a=r(!1),s=u(()=>a.value||e.eager||o.value);l(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{f as m,i as u};
