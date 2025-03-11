11c091cea56831ac3e83d6ea86c43a62a647be8b
Alban Mouton <alban.mouton@gmail.com>
2025-03-11 17:02:10 +0100

    Pushed latest by gh-pages-multi

--- latest/_nuxt/C1W_FP9n.js
+++
@@ -1,1 +0,0 @@
-import{a8 as t,o as e,j as s,ap as n}from"./D_Mb9exR.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
