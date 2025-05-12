364d6dcefc556d1aa989bb9291ca30cf46cbe3b1
Alban Mouton <alban.mouton@gmail.com>
2025-05-12 09:43:33 +0200

    Pushed master by gh-pages-multi

--- master/_nuxt/DS6NKgf3.js
+++
@@ -1,1 +0,0 @@
-import{a8 as t,o as e,j as s,ap as n}from"./DMJ_6nyb.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
