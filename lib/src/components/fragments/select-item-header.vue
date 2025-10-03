5195525ef74a95648eb016b4f664f1c80f073a31
Alban Mouton <alban.mouton@gmail.com>
2025-10-03 14:47:51 +0200

    feat: worj on select items of type header

---
+++ lib/src/components/fragments/select-item-header.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+import { VListSubheader } from 'vuetify/components/VList'
+import VSelectItemIcon from './select-item-icon.vue'
+
+defineProps({
+  item: {
+    /** @type import('vue').PropType<import('@json-layout/vocabulary').SelectItemHeader> */
+    type: Object,
+    required: true
+  },
+  avatarProps: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <v-list-subheader>
+    <v-select-item-icon
+      v-if="typeof item.icon === 'string'"
+      :icon="item.icon"
+      :avatar-props="avatarProps"
+    />
+    {{ item.title }}
+  </v-list-subheader>
+</template>
