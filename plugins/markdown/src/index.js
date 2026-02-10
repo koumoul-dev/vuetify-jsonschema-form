be59fcced7fc712fa09d2bacb9c913e6abbd3c6d
Alban Mouton <alban.mouton@gmail.com>
2026-02-10 16:53:55 +0100

    chore: add MarkdownEditor export

--- plugins/markdown/src/index.js
+++ plugins/markdown/src/index.js
@@ -1,6 +1,7 @@
 import { markRaw } from 'vue'
 import info from './info.js'
 import nodeComponent from './node.vue'
+export { default as MarkdownEditor } from './editor.vue'
 
 markRaw(nodeComponent)
 
