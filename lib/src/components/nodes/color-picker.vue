d92b4d0d5d299a419011b896794c5b658ee7dfbc
Alban Mouton <alban.mouton@gmail.com>
2026-02-17 11:10:39 +0100

    refactor: use VColorPicker from vuetify

--- lib/src/components/nodes/color-picker.vue
+++ lib/src/components/nodes/color-picker.vue
@@ -1,57 +1,38 @@
-<script setup>
-import TextFieldMenu from '../fragments/text-field-menu.vue'
-import { VColorPicker } from 'vuetify/components/VColorPicker'
-import { computed, toRef } from 'vue'
+<script>
+import { defineComponent, h, toRef, watch, computed } from 'vue'
+import { VColorInput } from 'vuetify/labs/VColorInput'
 import useNode from '../../composables/use-node.js'
 import { useDefaults } from 'vuetify'
+import useCompDefaults from '../../composables/use-comp-defaults.js'
 
-useDefaults({}, 'VjsfColorPicker')
-
-const props = defineProps({
-  modelValue: {
-    /** @type import('vue').PropType<import('../../types.js').VjsfColorPickerNode> */
-    type: Object,
-    required: true
+export default defineComponent({
+  props: {
+    modelValue: {
+      /** @type import('vue').PropType<import('../../types.js').VjsfColorPickerNode> */
+      type: Object,
+      required: true
+    },
+    statefulLayout: {
+      /** @type import('vue').PropType<import('../../types.js').VjsfStatefulLayout> */
+      type: Object,
+      required: true
+    }
   },
-  statefulLayout: {
-    /** @type import('vue').PropType<import('../../types.js').VjsfStatefulLayout> */
-    type: Object,
-    required: true
+  setup (props) {
+    useDefaults({}, 'VjsfColorPicker')
+    const vColorInputDefaults = useCompDefaults('VColorInput', { colorPip: true, hideActions: true })
+
+    const { inputProps, localData, compSlots } = useNode(
+      toRef(props, 'modelValue'), props.statefulLayout, { layoutPropsMap: ['placeholder'] }
+    )
+
+    const colorInputProps = computed(() => ({
+      ...vColorInputDefaults.value, ...inputProps.value, modelValue: localData.value
+    }))
+    watch(colorInputProps, () => console.log('colorInputProps', colorInputProps.value), { immediate: true })
+
+    return () => h(VColorInput, colorInputProps.value, compSlots.value)
   }
 })
 
-const { compProps, localData } = useNode(toRef(props, 'modelValue'), props.statefulLayout)
-
-const colorPickerProps = computed(() => {
-  const colorPickerProps = { ...compProps.value }
-  colorPickerProps.modelValue = localData.value
-  return colorPickerProps
-})
-
-const menuProps = computed(() => {
-  return {
-    minWidth: '300px',
-    maxWidth: '300px',
-  }
-})
 </script>
-
-<template>
-  <text-field-menu
-    :model-value="modelValue"
-    v-bind="menuProps"
-    :stateful-layout="statefulLayout"
-    :formatted-value="modelValue.data ?? undefined"
-  >
-    <template
-      v-if="modelValue.data"
-      #prepend-inner
-    >
-      <div :style="`height:30px; width: 30px; border-radius: 40px; margin-right:6px; background: ${modelValue.data};`" />
-    </template>
-    <v-color-picker
-      v-bind="colorPickerProps"
-      @update:model-value="value => statefulLayout.input(modelValue, value)"
-    />
-  </text-field-menu>
-</template>
